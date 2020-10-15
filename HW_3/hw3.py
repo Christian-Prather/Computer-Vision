37b500b2aa94e5b50ff82792771c071a36e149bb
Christian <christianprather@smilingrobotics.com>
2020-10-14 22:17:36 -0600

    CCC not very stable but better, getting order_list drop out

--- HW_3/hw3.py
+++ HW_3/hw3.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from order_targets import order_targets
 
 # Load in video
 video_capture = cv2.VideoCapture("fiveCCC.avi")
@@ -15,16 +16,16 @@ videoWriter = cv2.VideoWriter("fiveCCC_output.avi", fourcc=fourcc, fps=30.0, fra
 def distance(point_a, point_b):
     return np.sqrt((point_b[0]-point_a[0])**2 + (point_b[1]-point_a[1])**2)
 
-
 # Find CCC function pulled from past HW submission
 def find_CCC(frame, frame_number):
-
+    areas = []
     centroids_locations = []
+    points = []
     # frame = cv2.imread("CCCtarget.jpg")
 
     # Morph Configs
-    kernel_size_black = 1
-    kernel_size_white = 1
+    kernel_size_black = 2
+    kernel_size_white = 2
     kernel_black = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size_black, kernel_size_black))
     kernel_white = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size_white, kernel_size_white))
 
@@ -76,11 +77,36 @@ def find_CCC(frame, frame_number):
                     width_black = black_stat[cv2.CC_STAT_WIDTH]
                     height_black = black_stat[cv2.CC_STAT_HEIGHT]
                     if (x0_black < x0) and (y0_black < y0) and ((x0_black + width_black) > (x0 + width)) and ((y0_black + height_black) > (y0 + height)):
-                        if(black_stat[cv2.CC_STAT_AREA] < 300):
-                            centrod_image = cv2.rectangle(img=centrod_image, pt1=(x0_black,y0_black), pt2=(x0_black+width_black, y0_black+height_black), color=(0,255,0), thickness=1)
-                            print("Area", black_stat[cv2.CC_STAT_AREA])
+                        if((white_stat[cv2.CC_STAT_AREA] < 350) and (black_stat[cv2.CC_STAT_AREA] < 400)):
+                            # centrod_image = cv2.rectangle(img=centrod_image, pt1=(x0_black,y0_black), pt2=(x0_black+width_black, y0_black+height_black), color=(0,255,0), thickness=1)
+                            # print("Area", black_stat[cv2.CC_STAT_AREA])
                             # Append center of centroid to list
                             centroids_locations.append(np.array([(x0_black + 0.5*width_black) ,(y0_black + 0.5*height_black)]))
+                            areas.append(black_stat[cv2.CC_STAT_AREA])
+                            points.append(((x0_black,y0_black), (x0_black+width_black, y0_black+height_black)))
+    #Area filtering (needed due to issue with ccc false positives) 
+    # Reference https://medium.com/datadriveninvestor/finding-outliers-in-dataset-using-python-efc3fce6ce32
+    
+    if len(centroids_locations) > 5:
+        indexs = []
+        print(len(areas))
+        area_threshold = 3
+        mean = np.mean(areas)
+        std_area = np.std(areas)
+
+        for i in range(len(areas)):
+            z_score = (areas[i] - mean)/std_area
+            if np.abs(z_score) > area_threshold:
+                print("Removed...")
+                indexs.append(i)
+
+        indexs.sort(reverse=True)
+        for index in indexs:
+            _= centroids_locations.pop(index)
+            _ = points.pop(index)
+    print("P {} C {}".format(len(points), len(centroids_locations)))
+    # for point in points:
+    #     centrod_image = cv2.rectangle(img=centrod_image, pt1=point[0], pt2=point[1], color=(0,255,0), thickness=1)
 
     #Debugging Tools
     # cv2.imshow("Input", frame)
@@ -89,22 +115,32 @@ def find_CCC(frame, frame_number):
     # cv2.imshow("White Labels", labels_white_display)
     # cv2.imshow("Black Labels", labels_black_display)
     cv2.putText(centrod_image, "Frame: {}".format(str(frame_number)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-    cv2.imshow("Final", centrod_image)
-    cv2.waitKey(30)
+ 
 
     # print(centroids_locations)
     # print()
     ordered_centroids = order_targets(centroids_locations)
-    # print(ordered_centroids)
+    print(ordered_centroids)
+
+    for point in ordered_centroids:
+        print("Point", point[0])
+        centrod_image = cv2.rectangle(img=centrod_image, pt1=(int(point[0]) - 10, int(point[1]) -10), pt2=( int(point[0]) + 10, int(point[1]) + 10), color=(0,255,0), thickness=1)
+    # print(ordered_centrods)
     image_points = np.asarray(ordered_centroids, dtype=np.float32)
     # print(image_points)
     # print(image_points.shape)
     # Make sure ordering worked
-    if (len(ordered_centroids) != 5):
-        print("Error ordering Centroids...")
-        exit()
+    # if (len(ordered_centroids) != 5):
+    #     print("Error ordering Centroids...{}".format(len(ordered_centroids)))
+    #     exit()
+     # Print ordered labels
+    for i in range(len(ordered_centroids)):
+        point = (int(ordered_centroids[i][0]), int(ordered_centroids[i][1]))
+        # print(point)
+        cv2.putText(centrod_image, str(i), point, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,0,0), 1, cv2.LINE_AA)
 
-
+    cv2.imshow("Final", centrod_image)
+    cv2.waitKey(30)
 def main():
     frame_number = 1
 
